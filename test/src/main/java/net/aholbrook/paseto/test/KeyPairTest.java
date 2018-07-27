852910ad0b29ef0cb4752b169790c4bd012e9c8d
Andrew Holbrook <atholbro@gmail.com>
2018-07-26 20:06:26 -0400

    Add not equals test for keypair public key.

--- test/src/main/java/net/aholbrook/paseto/test/KeyPairTest.java
+++ test/src/main/java/net/aholbrook/paseto/test/KeyPairTest.java
@@ -19,10 +19,13 @@ public class KeyPairTest {
 	public void keyPair_notEquals() {
 		KeyPair kp1 = new KeyPair(RfcTestVectors.RFC_TEST_RSA_PRIVATE_KEY, RfcTestVectors.RFC_TEST_RSA_PUBLIC_KEY);
 		KeyPair kp2 = new KeyPair(RfcTestVectors.RFC_TEST_SK, RfcTestVectors.RFC_TEST_PK);
+		KeyPair kp3 = new KeyPair(RfcTestVectors.RFC_TEST_RSA_PRIVATE_KEY, RfcTestVectors.RFC_TEST_PK);
 		Assert.assertNotEquals(kp1, new Object());
 		Assert.assertEquals(false, kp1.equals(null));
 		Assert.assertEquals(false, kp1.equals(1));
 		Assert.assertNotEquals(kp1, kp2);
 		Assert.assertNotEquals(kp1.hashCode(), kp2.hashCode());
+		Assert.assertNotEquals(kp1, kp3);
+		Assert.assertNotEquals(kp1.hashCode(), kp3.hashCode());
 	}
 }
