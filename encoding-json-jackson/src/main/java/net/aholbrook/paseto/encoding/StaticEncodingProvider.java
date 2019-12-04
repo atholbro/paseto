8784e4b3bb2abe67cc20b4b07617b018c8962d0a
Andrew Holbrook <atholbro@gmail.com>
2019-12-04 15:13:59 -0500

    Import cleanup.

--- encoding-json-jackson/src/main/java/net/aholbrook/paseto/encoding/StaticEncodingProvider.java
+++ encoding-json-jackson/src/main/java/net/aholbrook/paseto/encoding/StaticEncodingProvider.java
@@ -1,6 +1,5 @@
 package net.aholbrook.paseto.encoding;
 
-import net.aholbrook.paseto.encoding.EncodingProvider;
 import net.aholbrook.paseto.encoding.json.jackson.JacksonJsonProvider;
 
 public class StaticEncodingProvider {
