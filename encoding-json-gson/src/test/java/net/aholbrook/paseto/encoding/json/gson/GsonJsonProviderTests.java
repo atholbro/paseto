baa5e27ef22ecf7e329eebaab93b78e6692397f5
Andrew Holbrook <atholbro@gmail.com>
2021-03-27 03:00:53 -0400

    OffsetDateTimeDeserializer tests.

--- encoding-json-gson/src/test/java/net/aholbrook/paseto/encoding/json/gson/GsonJsonProviderTests.java
+++ encoding-json-gson/src/test/java/net/aholbrook/paseto/encoding/json/gson/GsonJsonProviderTests.java
@@ -1,6 +1,7 @@
 package net.aholbrook.paseto.encoding.json.gson;
 
 import net.aholbrook.paseto.encoding.EncodingLoader;
+import net.aholbrook.paseto.time.OffsetDateTime;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,16 @@ public class GsonJsonProviderTests {
 	@Test
 	public void serviceLoader() {
 		Assertions.assertNotNull(EncodingLoader.getProvider(), "get provider");
+	}
+
+	@Test
+	public void offsetDateTimeAsObject() {
+		TestObject testObject = new GsonJsonProvider().decode("{\"time\":{\"time\":0}}", TestObject.class);
+		Assertions.assertNotNull(testObject);
+		Assertions.assertNull(testObject.time);
+	}
+
+	public static class TestObject {
+		public OffsetDateTime time;
 	}
 }
